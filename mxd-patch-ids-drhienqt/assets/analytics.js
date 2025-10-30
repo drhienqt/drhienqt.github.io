ee7cd1ebe4d521a17695db94e7f2ac1541e25670
drhienqt <drhienqt@gmail.com>
2025-10-30 23:34:02 +0900

    Add files via upload

---
+++ mxd-patch-ids-drhienqt/assets/analytics.js
@@ -0,0 +1,12 @@
+// MXD GA4 Loader — G-6BR46GX2XS
+(function(){ 
+  var gtagjs = document.createElement('script'); 
+  gtagjs.async = true; 
+  gtagjs.src = 'https://www.googletagmanager.com/gtag/js?id=G-6BR46GX2XS'; 
+  document.head.appendChild(gtagjs);
+  window.dataLayer = window.dataLayer || []; 
+  function gtag(){dataLayer.push(arguments);}
+  window.gtag = gtag;
+  gtag('js', new Date()); 
+  gtag('config', 'G-6BR46GX2XS'); 
+})();
