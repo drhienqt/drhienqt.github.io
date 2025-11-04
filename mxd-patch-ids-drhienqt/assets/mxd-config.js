5e110213d28bce8f1934c2e50c100dd192316c0e
drhienqt <drhienqt@gmail.com>
2025-11-05 00:29:25 +0900

    Delete mxd-patch-ids-drhienqt directory

--- mxd-patch-ids-drhienqt/assets/mxd-config.js
+++
@@ -1,7 +0,0 @@
-// MXD Affiliate Config — AccessTrade
-window.MXD_AFF = {
-  pub: "6822533979779947641",
-  shopee_cid: "4751584435713464237",
-  lazada_cid: "5127144557053758578",
-  tiktok_cid: "6648523843406889655"
-};
